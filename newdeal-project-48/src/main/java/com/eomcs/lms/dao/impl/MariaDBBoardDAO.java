06409291b9761900b57d6c1413da2bb917e224fa
zerocyber <zeroforever@naver.com>
2018-12-04 12:56:50 +0900

    change Statement to PreparedStatement

--- newdeal-project-48/src/main/java/com/eomcs/lms/dao/impl/MariaDBBoardDAO.java
+++ newdeal-project-48/src/main/java/com/eomcs/lms/dao/impl/MariaDBBoardDAO.java
@@ -2,6 +2,7 @@ package com.eomcs.lms.dao.impl;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -15,8 +16,8 @@ public class MariaDBBoardDAO implements BoardDAO{
   public List<Board> findAll() throws Exception {
     DriverManager.registerDriver(new Driver());
     try (Connection con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/studydb","study" ,"1111");
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("select bno, cont, cdt ,view, mno, lno from board");)
+        PreparedStatement stmt = con.prepareStatement("select bno, cont, cdt ,view, mno, lno from board");
+        ResultSet rs = stmt.executeQuery();)
     { 
       ArrayList<Board> list = new ArrayList<>(); 
 
@@ -37,23 +38,26 @@ public class MariaDBBoardDAO implements BoardDAO{
     DriverManager.registerDriver(new Driver());
     try(
         Connection con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/studydb","study" ,"1111");
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery(
+        PreparedStatement stmt = con.prepareStatement(
             "select bno, cont, cdt ,view, mno, lno"
                 + " from board"
-                + " where bno=" + no);
-        ) {
-      if(rs.next()) {
-        Board board = new Board();
-        board.setNo(rs.getInt("bno"));
-        board.setContents(rs.getString("cont"));
-        board.setCreatedDate(rs.getDate("cdt"));
-        board.setViewCount(rs.getInt("view"));
-        board.setWriterNo(rs.getInt("mno"));
-        board.setLessonNo(rs.getInt("lno"));
-        return board;
-      }else {
-        return null;
+                + " where bno=?");){
+
+      stmt.setInt(1, no);
+
+      try(ResultSet rs = stmt.executeQuery();){
+        if(rs.next()) {
+          Board board = new Board();
+          board.setNo(rs.getInt("bno"));
+          board.setContents(rs.getString("cont"));
+          board.setCreatedDate(rs.getDate("cdt"));
+          board.setViewCount(rs.getInt("view"));
+          board.setWriterNo(rs.getInt("mno"));
+          board.setLessonNo(rs.getInt("lno"));
+          return board;
+        }else {
+          return null;
+        }
       }
     }
   }
@@ -64,35 +68,33 @@ public class MariaDBBoardDAO implements BoardDAO{
         Connection con = DriverManager.getConnection(
             "jdbc:mariadb://localhost:3306/studydb",
             "study" ,"1111");
-        Statement stmt = con.createStatement();
-        ) {
+        PreparedStatement stmt = con.prepareStatement("insert into board(cont,mno,lno) values(?,?,?)" );) {
 
-      //SQL서버에 전송
-      return stmt.executeUpdate("insert into board(cont,mno,lno)"
-          + " values('"+board.getContents()+ "',"
-          + board.getWriterNo() + ","
-          + board.getLessonNo() + ")" );
+      stmt.setString(1, board.getContents());
+      stmt.setInt(2, board.getWriterNo());
+      stmt.setInt(3, board.getLessonNo());
+
+      return stmt.executeUpdate();
     } 
   }
 
   public int update(Board board) throws Exception {
     Connection con = null;;
-    Statement stmt =null;;
+    PreparedStatement stmt =null;;
 
-    // 이전 문법 방식 - 현업에서 JDBC를 쓴다면 이 방식을 더 많이 쓴다
     try {
       DriverManager.registerDriver(new Driver());
-
       con = DriverManager.getConnection(
           "jdbc:mariadb://localhost:3306/studydb",
           "study" ,"1111");
-      stmt = con.createStatement();
+      stmt = con.prepareStatement("update board set cont=? where bno=?");
+      
+      stmt.setString(1,board.getContents());
+      stmt.setInt(2, board.getNo());
 
-      return stmt.executeUpdate("update board set cont='"+
-          board.getContents() + 
-          "' where bno =" + board.getNo());
+      return stmt.executeUpdate();
 
-    }finally { // catch는 없어도 예외가 발생하면 알아서 에러를 띄워준다
+    }finally {
       try {stmt.close();}catch(Exception e) {}
       try {con.close();}catch(Exception e) {}
     }
@@ -100,17 +102,17 @@ public class MariaDBBoardDAO implements BoardDAO{
 
   public int delete(int no) throws Exception{
     Connection con = null;;
-    Statement stmt = null;;
+    PreparedStatement stmt = null;;
 
     try {
       DriverManager.registerDriver(new Driver());
       con = DriverManager.getConnection(
           "jdbc:mariadb://localhost:3306/studydb",
           "study" ,"1111");
-      stmt = con.createStatement();
-      
-      return stmt.executeUpdate("delete from board where bno=" + no);
-      
+      stmt = con.prepareStatement("delete from board where bno=?");
+      stmt.setInt(1, no);
+      return stmt.executeUpdate();
+
     }finally {
       try {stmt.close();}catch(Exception e) {}
       try {con.close();}catch(Exception e) {}
--- newdeal-project-48/src/main/java/com/eomcs/lms/dao/impl/MariaDBMemberDAO.java
+++ newdeal-project-48/src/main/java/com/eomcs/lms/dao/impl/MariaDBMemberDAO.java
@@ -2,8 +2,8 @@ package com.eomcs.lms.dao.impl;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import org.mariadb.jdbc.Driver;
 import com.eomcs.lms.dao.MemberDAO;
 import com.eomcs.lms.domain.Member;
@@ -14,24 +14,26 @@ public class MariaDBMemberDAO implements MemberDAO{
   public Member findByEmailPassword(String email, String password) throws Exception {
 
     DriverManager.registerDriver(new Driver());
-    try(
-        Connection con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/studydb","study" ,"1111");
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery(
-            "select mno,name,email,photo,tel"
-                + " from member"
-                + " where email='" + email
-                + "' and pwd='" + password + "'");
-        ) {
-      if(rs.next()) {
-        Member member = new Member();
-        member.setNo(rs.getInt("mno"));
-        member.setName(rs.getString("name"));
-        member.setEmail(rs.getString("email"));
-        member.setPhoto(rs.getString("photo"));
-        return member;
-      }else {
-        return null;
+
+
+    try(Connection con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/studydb","study" ,"1111");
+        PreparedStatement stmt = 
+            con.prepareStatement("select mno,name,email,photo, from member where email=? and pwd=?");)
+    {
+      stmt.setString(1, email);
+      stmt.setString(2, password);
+
+      try(ResultSet rs = stmt.executeQuery()){
+        if(rs.next()) {
+          Member member = new Member();
+          member.setNo(rs.getInt("mno"));
+          member.setName(rs.getString("name"));
+          member.setEmail(rs.getString("email"));
+          member.setPhoto(rs.getString("photo"));
+          return member;
+        }else {
+          return null;
+        }
       }
     }
   }
